bfd22216f31bc91e4f2395c00344bbc3c5cbe455
Archana0427 <archana0chowdary@gmail.com>
2026-03-05 15:11:25 +0530

    UC3 coded to console

--- src/printoops.java
+++ src/printoops.java
@@ -1,9 +1,9 @@
 public class printoops{
     public static void main(String[] args){
-        System.out.println("  ***  " + "  " + " ***** " + "  " + " ***** " + "  " + " ***** ");
-        System.out.println(" *   * " + "  " + " *   * " + "  " + " *   * " + "  " + " *     ");
-        System.out.println(" *   * " + "  " + " *   * " + "  " + " * * * " + "  " + " ***** ");
-        System.out.println(" *   * " + "  " + " *   * " + "  " + " *     " + "  " + "     * ");
-        System.out.println("  ***  " + "  " + " ***** " + "  " + " *     " + "  " + " ***** ");
+        System.out.println(String.join("   ","*****","*****" ,"*****" ,"*****"));
+        System.out.println(String.join("   ","*   *","*   *" ,"*   *" ,"*    "));
+        System.out.println(String.join("   ","*   *","*   *" ,"* * *" ,"*****"));
+        System.out.println(String.join("   ","*   *","*   *" ,"*    " ,"    *"));
+        System.out.println(String.join("   ","*****","*****" ,"*    " ,"*****"));
     }
 }
